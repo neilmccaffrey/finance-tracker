bc13549221291ab3652e22a6fab61e5bf72425fe
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2025-02-06 10:42:10 -0500

    added register user auth

---
+++ src/api/auth.js
@@ -0,0 +1,11 @@
+const API_URL = 'http://localhost:5000/api/users'; // Adjust for production
+
+export const registerUser = async (username, password) => {
+  const response = await fetch(`${API_URL}/register`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, passwordHash: password }),
+  });
+
+  return response.json();
+};
