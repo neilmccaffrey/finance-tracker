c8ff8d062eaaf8ddb6c9857895bf7abb4118c02a
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2025-02-13 14:12:39 -0500

    updated api url to be prefaced with REACT_APP_

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,6 @@
-const API_URL = process.env.API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const registerUser = async (username, password) => {
-  console.log('API_URL', process.env.API_URL);
   const response = await fetch(`${API_URL}/api/users/register`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
