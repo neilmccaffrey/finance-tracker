8a6e8fd3f8f5da7e053d62f567864e19ec9f4b05
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2025-02-13 17:47:10 -0500

    changed id, title, url

--- src/components/FinancialNews.jsx
+++ src/components/FinancialNews.jsx
@@ -19,10 +19,10 @@ const FinancialNews = () => {
   return (
     <div className="flex flex-col dark:bg-gray-500 p-2 rounded shadow-md mt-2">
       {articles.map((article) => (
-        <div key={article.Id} className="p-2 border-b border-gray-300">
-          <h3 className="font-semibold text-sm">{article.Title}</h3>
+        <div key={article.id} className="p-2 border-b border-gray-300">
+          <h3 className="font-semibold text-sm">{article.title}</h3>
           <a
-            href={article.Url}
+            href={article.url}
             target="_blank"
             rel="noopener noreferrer"
             className="text-blue-500 underline"
