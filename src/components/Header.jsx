c447c85cb90bdae220c9b6e30b26802fbd9d770e
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2025-02-14 14:36:03 -0500

    added home button to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import ThemeToggle from './ThemeToggle';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 import { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
@@ -12,13 +12,19 @@ const Header = () => {
     <header className="w-full fixed top-0 p-2 md:p-6">
       <div className="flex justify-between items-center w-full md:space-x-96">
         <ThemeToggle />
-        <div className="inline-flex gap-x-2 items-center transition-transform duration-200 hover:scale-125 mr-4">
-          <FontAwesomeIcon icon={faUser} />
-          {!token ? (
-            <Link to="/login">Login/Register</Link>
-          ) : (
-            <button onClick={handleLogout}>Logout</button>
-          )}
+        <div>
+          <div className="inline-flex gap-x-2 items-center transition-transform duration-200 hover:scale-125 mr-4">
+            <FontAwesomeIcon icon={faHome} />
+            <Link to="/">Home</Link>
+          </div>
+          <div className="inline-flex gap-x-2 items-center transition-transform duration-200 hover:scale-125 mr-4">
+            <FontAwesomeIcon icon={faUser} />
+            {!token ? (
+              <Link to="/login">Login/Register</Link>
+            ) : (
+              <button onClick={handleLogout}>Logout</button>
+            )}
+          </div>
         </div>
       </div>
     </header>
